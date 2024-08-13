f911f8a6d15ae06029fec0b583b62bd7fbf00952
Chidi Umenwofor-Nweze <chidiumenwofor-nweze@Chidis-MacBook-Pro.local>
2024-08-13 15:50:31 -0400

    created shell of a chat

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Agent from './src';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Agent/>
     </View>
   );
 }
@@ -14,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/index.js
@@ -0,0 +1,59 @@
+import { View, Text } from "react-native";
+import { React, useState } from "react";
+import { GiftedChat } from 'react-native-gifted-chat';
+import { Axios } from "axios";
+
+const Agent = () => {
+    const [messages, setMessages] = useState([])
+
+    const VOICEFLOW_API_KEY = 'VF.DM.66bbadc24291262cfd18631f.r3OasGOIa8LInKBr'
+
+    const handleSend = async (newMessages = []) => {
+            // Get the user's message
+            const userMessage = newMessages[0];
+
+            // Add the user's message to the messages state
+            setMessages(previousMessages => GiftedChat.append(previousMessages, userMessage)); 
+            const messageText = userMessage.text.toLowerCase();
+
+            // Agent message back
+            // TO-DO: Add call to DM API (using axios)
+            const agentMessage = {
+                _id: new Date().getTime() + 1,
+                text: "Helloooooo!",
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'Hotel Assistant',
+                }
+            }
+            setMessages(previousMessages => GiftedChat.append(previousMessages, agentMessage));
+            return;
+    }
+
+    return (
+        <View style={{ flex:1 }}>
+            <View style={{
+                backgroundColor: '#f8f5eb',
+                padding:10,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderBottomWidth:1,
+                marginTop: 40,
+                marginBottom: 200,
+            }}>
+                <Text style={{
+                    fontSize: 32,
+                    fontWeight: 'bold'
+                }}>Voiceflow Agent on Mobile!</Text>
+            </View>
+            <GiftedChat 
+                messages={messages}
+                onSend={messages => handleSend(messages)}
+                user={{ _id:1}}
+            />
+        </View>
+    )
+}
+
+export default Agent;
