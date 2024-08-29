20abc5653da29fcad74090aa047ae9857c763407
ChidiNweze <c.umenwofornweze@gmail.com>
2024-08-29 14:50:54 -0400

    Typing indicator cleanup

--- src/TypingIndicator.js
+++ src/TypingIndicator.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { View, Text } from "react-native";
-import './TypingIndicator.css'; // You'll style this component in this file
 
 const TypingIndicator = () => (
   <View className="typing-indicator">
-    <Text></Text>
+    <Text>{"..."}</Text>
   </View>
 );
 
